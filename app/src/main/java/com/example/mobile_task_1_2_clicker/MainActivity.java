2f1a10fc9390c81a5566974e389a7b707e83c4ad
Nastya <nasti.zaitseva@yandex.ru>
2019-04-03 18:09:22 +0300

    Прошлый кликер

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -4,36 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextClock;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView mainText;
-    Button mainBtn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private long score = 0;
+    TextView mainTextView;
+    Button btn1;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainText = (TextView) findViewById(R.id.mainText);
-        mainBtn = (Button) findViewById(R.id.button);
+        btn1 = findViewById(R.id.btn);
+        btn1.setOnClickListener(this);
 
-
-        View.OnClickListener clickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
-                mainText.setText(s.toCharArray(),0, s.length());
-            }
-        };
-
-        mainBtn.setOnClickListener(clickListener);
-
+        mainTextView = findViewById(R.id.main_textview);
     }
+
+
+
+    @Override
+    public void onClick (View v){
+        mainTextView.setText(String.valueOf(++count));
+    }
+
+
+
+
+
 }
 
