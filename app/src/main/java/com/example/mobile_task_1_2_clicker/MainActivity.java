f7aa5f7f31a4dc4dd8f4c06eb00de8d94e7a179e
Nastya <nasti.zaitseva@yandex.ru>
2019-04-05 17:03:04 +0300

    Очищение кода

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -2,70 +2,25 @@ package com.example.mobile_task_1_2_clicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    TextView mainTextView;
-    Button btn1;
-    Button btn_minus;
-    Button btn_null;
-    int count = 0;
-    ImageButton imageButton;
+public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btn);
-        btn1.setOnClickListener(this);
 
-        btn_minus =findViewById(R.id.btn_minus);
-        btn_minus.setOnClickListener(this);
-
-        btn_null =findViewById(R.id.btn_null);
-        btn_null.setOnClickListener(this);
-
-        mainTextView = findViewById(R.id.main_textview);
-
-        imageButton=findViewById(R.id.imageButton);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageButton.setImageResource(R.drawable.robot2);
 
             }
-        });
 
 
 
-    }
 
-
-
-    @Override
-    public void onClick (View v) {
-        switch (v.getId()) {
-            case R.id.btn:
-                mainTextView.setText(String.valueOf(++count));
-                break;
-
-            case R.id.btn_minus:  if (count>0){
-                mainTextView.setText(String.valueOf(--count));
-                break;}
-
-            case R.id.btn_null:  mainTextView.setText(String.valueOf(count=0));
-                break;
-
-
-        }
-    }
 
 
 }
